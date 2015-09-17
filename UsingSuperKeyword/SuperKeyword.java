581a4a556850f66cf6ee45e6ded513f99e81332d
raczlaszlo <raczl@live.com>
2015-09-17 11:39:24 +0200

    Created by Coding Ground Client

---
+++ UsingSuperKeyword/SuperKeyword.java
@@ -0,0 +1,10 @@
+public class SuperKeyword
+{
+    public static void main(String[] args)
+    {
+        SubClass mainApp = new SubClass();
+        mainApp.callerMethod();
+        
+        
+    }
+}
