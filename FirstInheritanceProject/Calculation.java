24f28efda2c71e326b2816d727dff1e29843f6bb
raczlaszlo <raczl@live.com>
2015-09-17 11:14:02 +0200

    Created by Coding Ground Client

---
+++ FirstInheritanceProject/Calculation.java
@@ -0,0 +1,11 @@
+public class Calculation
+{
+    int z;
+    public void add(int a, int b)
+    {
+        z = a+b;
+        System.out.println("The sum of "+a+" and "+b+" is "+z+".");
+    }
+    
+    
+}
