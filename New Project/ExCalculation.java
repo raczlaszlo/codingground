271257316e370e17aabb1b7626955de7fa25d555
raczlaszlo <raczl@live.com>
2015-09-17 11:10:33 +0200

    Created by Coding Ground Client

---
+++ New Project/ExCalculation.java
@@ -0,0 +1,11 @@
+public class ExCalculation extends Calculation
+{
+    int z;
+    public void multi(int a, int b)
+    {
+        z = a*b;
+        System.out.println("The product of "+a+" and "+b+" is "+z+".");
+    }
+    
+    
+}
